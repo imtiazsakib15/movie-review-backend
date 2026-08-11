e264b31f37f89d03c704b36536569a4db9b84f86
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2026-08-12 05:27:10 +0600

    register api response updated

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,19 @@ import globalErrorHandler from "./middlewares/globalErrorHandler";
 import cookieParser from "cookie-parser";
 import notFound from "./middlewares/notFound";
 import helmet from "helmet";
+import { env } from "./config/env";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1", router);
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,11 +2,31 @@ import { Request, Response } from "express";
 import { authService } from "./auth.service";
 import catchAsync from "../../utils/catchAsync";
 import { sendSuccess } from "../../utils/apiResponse";
+import { env } from "../../config/env";
 
 export const authController = {
   register: catchAsync(async (req: Request, res: Response) => {
     const result = await authService.register(req.body);
-    sendSuccess(res, 201, "Account created successfully", result);
+
+    const tokens = result.tokens;
+    const accessTokenMaxAge = 1 * 24 * 60 * 60 * 1000;
+    const refreshTokenMaxAge = 7 * 24 * 60 * 60 * 1000;
+
+    res
+      .cookie("accessToken", tokens.accessToken, {
+        httpOnly: true,
+        secure: env.NODE_ENV === "production",
+        sameSite: env.NODE_ENV === "production" ? "none" : "lax",
+        maxAge: accessTokenMaxAge,
+      })
+      .cookie("refreshToken", tokens.refreshToken, {
+        httpOnly: true,
+        secure: env.NODE_ENV === "production",
+        sameSite: env.NODE_ENV === "production" ? "none" : "lax",
+        maxAge: refreshTokenMaxAge,
+      });
+
+    sendSuccess(res, 201, "Account created successfully", result.user);
   }),
 
   login: catchAsync(async (req: Request, res: Response) => {
