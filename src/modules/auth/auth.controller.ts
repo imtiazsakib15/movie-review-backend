2f004d87a75c452dde4bf73bb3ff7d882b7ecb93
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2026-08-12 06:17:22 +0600

    login api response updated

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -31,7 +31,25 @@ export const authController = {
 
   login: catchAsync(async (req: Request, res: Response) => {
     const result = await authService.login(req.body);
-    sendSuccess(res, 200, "Logged in successfully", result);
+
+    const tokens = result.tokens;
+    const accessTokenMaxAge = 1 * 24 * 60 * 60 * 1000;
+    const refreshTokenMaxAge = 7 * 24 * 60 * 60 * 1000;
+
+    res
+      .cookie("accessToken", tokens.accessToken, {
+        httpOnly: true,
+        secure: env.NODE_ENV === "production",
+        sameSite: env.NODE_ENV === "production" ? "none" : "lax",
+        maxAge: accessTokenMaxAge,
+      })
+      .cookie("refreshToken", tokens.refreshToken, {
+        httpOnly: true,
+        secure: env.NODE_ENV === "production",
+        sameSite: env.NODE_ENV === "production" ? "none" : "lax",
+        maxAge: refreshTokenMaxAge,
+      });
+    sendSuccess(res, 200, "Logged in successfully", result.user);
   }),
 
   me: catchAsync(async (req: Request, res: Response) => {
